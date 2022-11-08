98bcd2d6e77e94a2412ea675544510ee38b88702
akneeehow <113169635+akneeehow@users.noreply.github.com>
2022-11-08 11:22:00 +0000

    Add files via upload

---
+++ Challenge5.java
@@ -0,0 +1,8 @@
+public class Challenge5 {
+    public static void main(String[] args) {
+        double numb = Integer.parseInt(args[0]);
+        double numb2 = Integer.parseInt(args[1]);
+        double total = Math.pow(numb, numb2);
+        System.out.println(total);
+    }
+}
