95dcc0431b101f1045e7acd6ac017dac6a6264cd
akneeehow <113169635+akneeehow@users.noreply.github.com>
2022-11-10 10:04:13 +0000

    Add files via upload

---
+++ Challenge3.java
@@ -0,0 +1,12 @@
+public class Challenge3 {
+    public static void main(String[] args){
+        int total = Integer.parseInt(args[0]);
+        for(int i=0;i<13;i++){
+            int mul = total * i;
+            System.out.println(mul);
+
+        }
+
+    }
+}
+
